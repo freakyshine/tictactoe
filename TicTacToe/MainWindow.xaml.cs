f03b4a7bce49fe2d99de9ad1e01a21d10d757b47
Daniel Lutz <daniel.lutz@mobilezoneholding.net>
2021-04-09 21:14:15 +0200

    Adding constructor for Player, adding Username property, adding Game constructor

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -63,6 +63,18 @@ namespace TicTacToe
         /// The Grid object that holds the grid with all fields
         /// </summary>
         public Grid Game_Grid { get; set; }
+
+        /// <summary>
+        /// Constructs a Game object
+        /// </summary>
+        /// <param name="p1">The username for player one</param>
+        /// <param name="p2">The username for player two</param>
+        public Game(string p1, string p2)
+        {
+            Player_One = new Player(p1);
+            Player_Two = new Player(p2);
+            Game_Grid = new Grid();
+        }
     }
 
     /// <summary>
@@ -70,7 +82,18 @@ namespace TicTacToe
     /// </summary>
     public class Player
     {
-
+        /// <summary>
+        /// This is the player's username. It can only be defined through the constructor.
+        /// </summary>
+        public string Username { get; private set; }
+        /// <summary>
+        /// Constructs a Player object
+        /// </summary>
+        /// <param name="username">The username for the player to be constructed</param>
+        public Player(string username)
+        {
+            Username = username;
+        }
     }
 
     /// <summary>
@@ -84,7 +107,7 @@ namespace TicTacToe
         public int[,] Grid_Matrix { get; set; }
 
         /// <summary>
-        /// This constructs a Grid
+        /// Constructs a Grid
         /// </summary>
         /// <param name="grid_size">This defines how big the grid is. Grid size is in square.</param>
         public Grid(int grid_size = 3)
